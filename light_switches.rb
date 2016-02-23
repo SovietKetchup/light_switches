ec3aed8bbf6acb6f9b487387a7de9440398a676d
SovietKetchup <sam.comer+git@gmail.com>
2016-02-23 15:39:48 +0000

    Added array counting. Program completed

--- light_switches.rb
+++ light_switches.rb
@@ -9,20 +9,18 @@ lights = Array.new(input.readline.to_i, 0)
 lines = File.foreach("nums.txt").count.to_i - 1
 lines.times {
   # Getting the range to swap
-  range = input.readline.split(" ")
-  range[0] = range[0].to_i; range[1] = range[1].to_i
-  
-  # Setting lower and upper ranges
-  if range[0] <= range[1]
-    rl = range[0]; ru = range[1]
+  ran = input.readline.split(" ")
+
+  if ran.first.to_i <= ran.last.to_i
+    rl = ran.first.to_i; ru = ran.last.to_i
   else
-    rl = range[1]; ru = range[0]
+    rl = ran.last.to_i; ru = ran.first.to_i
   end
   
   lights.each_with_index{ |v, i| lights[i] = (lights[i]-1).abs if i >= rl && i <= ru }
 }
-
-puts lights
+f.close
+puts lights.count(1)
 
 
 
